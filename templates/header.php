03fe609b0dd5550c517550e305676688f6d40852
Miguel Ortiz <miguel.ortiz.q55@gmail.com>
2024-01-04 15:55:16 -0300

    Ajuste el header, dlt dir

--- templates/header.php
+++ templates/header.php
@@ -15,54 +15,42 @@
 </head>
 
 <body>
-    <section class="ftco-section">
-        <div class="wrap">
-            <div class="container">
-                <div class="row justify-content-between">
-                </div>
-            </div>
-        </div>
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-        <a class="navbar-brand" href="index.html">Control de Inventario Interno<span>DYC</span></a>
-        <form action="#" class="searchform order-sm-start order-lg-last">
-            <div class="form-group d-flex">
-                <!-- Futura implementación
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light mt-2" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">Control de Inventario Interno<span>DYC Ingenieria y Automatizacion</span></a>
+            <form action="#" class="searchform order-sm-start order-lg-last">
+                <div class="form-group d-flex">
+                    <!-- Futura implementación
                 <input type="text" class="form-control pl-3" placeholder="Search">
                 <button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button> -->
+                </div>
+            </form>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars"></span> Menu
+            </button>
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav m-auto">
+                    <li class="nav-item active"><a href="../public/main.php" class="nav-link">Ir al inicio</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Productos</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown04">
+                            <a class="dropdown-item" href="#">Ingreso producto</a>
+                            <a class="dropdown-item" href="#">Salida producto</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stock</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown02">
+                            <a class="dropdown-item" href="#">Stock General</a>
+                            <a class="dropdown-item" href="#">Stock por Producto</a>
+                            <a class="dropdown-item" href="#">Stock por Proveedor</a>
+                            <a class="dropdown-item" href="#">Stock por Categoria</a>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </form>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="fa fa-bars"></span> Menu
-        </button>
-        <div class="collapse navbar-collapse" id="ftco-nav">
-            <ul class="navbar-nav m-auto">
-                <li class="nav-item active"><a href="#" class="nav-link">Inicio</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Productos</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown04">
-                        <a class="dropdown-item" href="#">Ingreso producto</a>
-                        <a class="dropdown-item" href="#">Salida producto</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stock</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown02">
-                        <a class="dropdown-item" href="#">Stock General</a>
-                        <a class="dropdown-item" href="#">Stock por Producto</a>
-                        <a class="dropdown-item" href="#">Stock por Proveedor</a>
-                        <a class="dropdown-item" href="#">Stock por Categoria</a>
-                    </div>
-                </li>
-            </ul>
         </div>
-    </div>
-</nav>
-
-        <!-- END nav -->
-
-    </section>
-
+    </nav>
     <script src="../js/jquery.min.js"></script>
     <script src="../js/popper.js"></script>
     <script src="../js/bootstrap.min.js"></script>
