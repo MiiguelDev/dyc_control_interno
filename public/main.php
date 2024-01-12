88e1fcac976028c9a081d3b4c03e593031af1b96
Miguel Ortiz <miguel.ortiz.q55@gmail.com>
2024-01-12 11:35:20 -0300

    Ajustes en la visualizacion del main menu

--- public/main.php
+++ public/main.php
@@ -24,25 +24,26 @@ $is_admin = isUserAdmin();
     <?php include '../templates/header.php'; ?>
     <div class="container-fluid text-center">
         <div class="row content">
-            <div class="col-sm-2 sidenav pt-5">
+            <!-- A implementar en el futuro -->
+            <!-- <div class="col-sm-2 sidenav pt-5">
                 <p><a href="#">Mis ingresos</a></p>
                 <p><a href="#">Mis salidas</a></p>
                 <p><a class="disabled-link" href="#">Registro de mis servicios</a></p>
-            </div>
+            </div> -->
             <div class="col-sm-8 text-left pt-4">
-                <h1>Bienvenido 
-                    <?php if ($is_admin) : ?> Administrador<?php endif; ?></h1>
-                <p>Aca iran listados eventualmente los ultimos trabajos realizados de manera modular ordenados por fecha</p>
+                <h2>Bienvenido 
+                    <?php if ($is_admin) : ?> Administrador<?php endif; ?></h2>
+                <p>Aca iran listados eventualmente los ultimos trabajos realizados del usuario de manera modular ordenados por fecha</p>
                 <hr>
                 <h3>Segundo bloque de contenido</h3>
                 <p>Aca iran listados indicaciones generales que se hayan efectuado en la semana, ordenadas por fecha, hora y quien lo indico.</p>
             </div>
             <div class="col-sm-2 sidenav">
                 <div class="well">
-                    <p class="pt-4" >Contenido a especificar 1</p>
+                    <p class="pt-4" >Fecha actual con alguna indicacion del tiempo de las zonas en que se esta trabajando</p>
                 </div>
                 <div class="well">
-                    <p class="pt-4" >Contenido a especificar 2</p>
+                    <p class="pt-4" >Contenido a agregar eventualmente, sin definir</p>
                 </div>
             </div>
         </div>
